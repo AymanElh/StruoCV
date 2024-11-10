4895ba73ac0a464ea1822d563bae22e917346048
unknown <elhamioui1964@gmail.com>
2024-11-10 23:17:08 +0100

    Edit the social links div and add addLinkField function

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,3 +21,35 @@ prevBtns.forEach(btn => {
     formSteps[counter].classList.replace("hidden", "flex");
   })
 })
+
+// Function to add a social link on contact information step.
+let linkCounter = 0;
+function addLinkField() {
+  linkCounter++;
+
+  // create the container of the link
+  const linkContainer = document.createElement("div");
+  linkContainer.id = `link${linkCounter}`
+
+  // create input for the link
+  const linkInput = document.createElement("input");
+  linkInput.type = "text";
+  linkInput.placeholder = "Enter your social link eg. github, linkedin ..."
+  linkInput.classList.add("text-sm", "rounded-lg",  "block", "w-full", "p-2.5", "bg-gray-700", "border-gray-600", "placeholder-gray-400", "text-white", "focus:ring-blue-500", "focus:border-blue-500");
+
+
+  // remove button
+  const removeBtn = document.createElement("button");
+  removeBtn.type = "button";
+  removeBtn.innerText = "Remove";
+  removeBtn.classList.add("text-white", "focus:outline-none", "focus:ring-4", "font-medium", "rounded-lg", "text-sm", "px-5", "py-2.5", "me-2", "mb-2", "bg-gray-800", "hover:bg-gray-700", "focus:ring-gray-700", "border-gray-700", "mt-3");
+  removeBtn.onclick = function() {
+    removeLink(linkContainer.id);
+  }
+
+  // Save the link to the html
+  linkContainer.appendChild(linkInput);
+  linkContainer.appendChild(removeBtn);
+
+  document.getElementById("social-links").appendChild(linkContainer);
+}
